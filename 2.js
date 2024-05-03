04725de9cce11d09826869a1eb0aa4d71b7cf05c
balakrishnan4591 <balakrishnan4591@gmail.com>
2024-05-04 03:34:54 +0530

    Day 6 Assignment

--- 2.js
+++ 2.js
@@ -16,3 +16,42 @@
 +getCircumference(): double
 
 */
+
+class Circle {
+  constructor(radius, color) {
+    this.radius = radius;
+    this.color = color;
+  }
+
+  //   getRadius() {
+  //     return this.radius;
+  //   }
+
+  //   setRadius() {
+  //     this.radius = this.radius;
+  //   }
+
+  //   getColor() {
+  //     return this.color;
+  //   }
+
+  //   setColor() {
+  //     this.color = this.color;
+  //   }
+
+  getArea(radius) {
+    var radius = this.radius;
+    var area = (22 / 7) * (radius * radius);
+    return area.toFixed(2);
+  }
+
+  getCircumference(radius) {
+    var radius = this.radius;
+    var circumference = 2 * (22 / 7) * radius;
+    return circumference.toFixed(2);
+  }
+}
+
+var circle = new Circle(3, "red");
+console.log("Area of give circle is : " + circle.getArea());
+console.log("Circumference of give circle is : " + circle.getCircumference());
